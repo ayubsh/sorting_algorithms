4208b4ec1a71d5537c173d6b247f7dbf7b5c6dd8
ayubsh <ibrahimayubsheikh@gmail.com>
2022-11-06 14:49:27 +0300

    fixed std=89

--- 0-main.c
+++ 0-main.c
@@ -11,7 +11,6 @@ int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
-    printf("size is %ld\n", n);
 
     print_array(array, n);
     printf("\n");
