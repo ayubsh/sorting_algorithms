ea8c38c44aaa5610d67a1df14854627eda79debe
ayubsh <ibrahimayubsheikh@gmail.com>
2022-11-06 14:44:22 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - impliments bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+
+			if (array[j + 1] < array[j])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Sorting Algorithms
+contains Sorting Algorithms and big-oh notation tasks
+
+- big-oh
+- theta
+- omega
+- bubble sort
+- merge sort
+- insertion sort
+- selection sort
+- quick sort
+
